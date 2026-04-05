f8b29501ec4882681d33a3d5ba5d32dc99954f20
Vitaliy <chernush.vitaly@gmail.com>
2026-04-05 14:08:46 +0200

    Options restructuring

--- FadeBlizzardBars.lua
+++ FadeBlizzardBars.lua
@@ -92,11 +92,10 @@ return {
     }
 end
 
-local function GetOptionConfig(label, barKey, order)
+local function GetOptionGroupConfig(label, barKey, order)
     return {
         name = label,
         type = "group",
-        inline = true,
         args = GetOptionConfigArgs(barKey),
         order = order,
     }
@@ -106,14 +105,10 @@ end
 local options = {
     name = "FadeBlizzardBars",
     type = "group",
+    childGroups = "tree",
     args = {
-        header = {
-            name = "FadeBlizzardBars",
-            type = "header",
-            order = 0,
-        },
         configOptions = {
-            name = "",
+            name = "General settings",
             type = "group",
             inline = true,
             order = 1,
@@ -153,18 +148,18 @@ local options = {
             type = "header",
             order = 2,
         },
-        mainBarOption = GetOptionConfig("Main Action Bar", "mainActionBar", 3),
-        bottomLeftBarOption = GetOptionConfig("Bottom Left Action Bar", "multiBarBottomLeft", 4),
-        bottomRightBarOption = GetOptionConfig("Bottom Right Action Bar", "multiBarBottomRight", 5),
-        rightBarOption = GetOptionConfig("Right Action Bar", "multiBarRight", 6),
-        leftBarOption = GetOptionConfig("Left Action Bar", "multiBarLeft", 7),
-        multiBar5Option = GetOptionConfig("Multi Bar 5", "multiBar5", 8),
-        multiBar6Option = GetOptionConfig("Multi Bar 6", "multiBar6", 9),
-        multiBar7Option = GetOptionConfig("Multi Bar 7", "multiBar7", 10),
-        petBarOption = GetOptionConfig("Pet Action Bar", "petActionBar", 11),
-        stanceBarOption = GetOptionConfig("Stance Bar", "stanceBar", 12),
-        bagsBarOption = GetOptionConfig("Bags Bar", "bagsBar", 13),
-        microMenuOption = GetOptionConfig("Micro Menu", "microMenuContainer", 14),
+        mainBarOption = GetOptionGroupConfig("Main Action Bar", "mainActionBar", 3),
+        bottomLeftBarOption = GetOptionGroupConfig("Bottom Left Action Bar", "multiBarBottomLeft", 4),
+        bottomRightBarOption = GetOptionGroupConfig("Bottom Right Action Bar", "multiBarBottomRight", 5),
+        rightBarOption = GetOptionGroupConfig("Right Action Bar", "multiBarRight", 6),
+        leftBarOption = GetOptionGroupConfig("Left Action Bar", "multiBarLeft", 7),
+        multiBar5Option = GetOptionGroupConfig("Multi Bar 5", "multiBar5", 8),
+        multiBar6Option = GetOptionGroupConfig("Multi Bar 6", "multiBar6", 9),
+        multiBar7Option = GetOptionGroupConfig("Multi Bar 7", "multiBar7", 10),
+        petBarOption = GetOptionGroupConfig("Pet Action Bar", "petActionBar", 11),
+        stanceBarOption = GetOptionGroupConfig("Stance Bar", "stanceBar", 12),
+        bagsBarOption = GetOptionGroupConfig("Bags Bar", "bagsBar", 13),
+        microMenuOption = GetOptionGroupConfig("Micro Menu", "microMenuContainer", 14),
     },
 }
 
